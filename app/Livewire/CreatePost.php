980043c6679b455b2377571c6b24eed2f9f404f9
jakebuceac <buceacjake@gmail.com>
2023-11-02 14:25:36 +0000

    Finished fifth lesson

--- app/Livewire/CreatePost.php
+++ app/Livewire/CreatePost.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Livewire\Forms\PostForm;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class CreatePost extends Component
@@ -27,5 +28,10 @@ class CreatePost extends Component
     public function validateTitle(): void
     {
         $this->validateOnly('form.title');
+    }
+
+    public function updatedFormTitle(): void
+    {
+        $this->form->title = Str::headline($this->form->title);
     }
 }
